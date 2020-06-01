67ae962d8cad04631ed7c33e485d968885d1d4c0
Alex <aperacaularuiz@gmail.com>
2020-06-01 17:37:47 +0200

    More footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,15 +1,56 @@
 import React from 'react';
 import styles from './footer.module.scss';
+import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const Footer = () => (
+const Footer = () => {
+    const icons = useStaticQuery(graphql`
+    query {
+      instagram: file(relativePath: { eq: "instagram-icon.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      mail: file(relativePath: { eq: "email-icon.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      linkedin: file(relativePath: { eq: "linkedin-icon.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+    return (
     <footer>
-        <div>
+        <div className={styles.first_container}>
             © {new Date().getFullYear()}, by
             {` `}
-            <a href="">Àlex Peracaula Ruiz</a>
+            Àlex Peracaula Ruiz
+            <div className={styles.logos_container}>
+                <div className={styles.mail}>
+                    <a href="mailto:aperacaularuiz@gmail.com" target="blank"><Img fluid={icons.mail.childImageSharp.fluid} /></a>
+                </div>
+                <div className={styles.instagram}>
+                    <a href="https://www.instagram.com/aperacaula" target="blank"><Img fluid={icons.instagram.childImageSharp.fluid} /></a>
+                </div>
+                <div className={styles.linkedin}>
+                    <a href="https://www.linkedin.com/in/alex-peracaula-9b4b1b11a/" target="blank"><Img fluid={icons.linkedin.childImageSharp.fluid} /></a>
+                </div>
+            </div>
         </div>
     </footer>
-)
+    )
+}
 
 
 export default Footer;
--- src/pages/sobre-mi.jsx
+++ src/pages/sobre-mi.jsx
@@ -8,36 +8,36 @@ import styles from '../styles/sobremi.module.scss'
 
 const SecondPage = (props) => (
   <Layout page={'sobremi'} >
-    <SEO title="Page two" />
+    <SEO title="Sobre mí" />
     <h1>Sobre mí</h1>
     <p>Me llamo Àlex Peracaula Ruiz, y nací el 7 de octubre de 1993 en Barcelona,
-     donde actualmente resido. Mi pasión por la interpretación nació muy temprano.
+     donde actualmente resido. Mi pasión por la interpretación empezó muy temprano.
      Por influencia mayormente de mi abuelo, empecé a hacer teatro en la escuela y 
      en el grupo de teatro amateur "La Teatral", perteneciente a "La Caixa", donde
      fui acumulando experiencia con papeles cada vez más complejos.</p>
  
      <p>Manteniendo el teatro como un hobbie, aunque necesario, estudié Ingeniería Industrial
        en la Universitat Politècnica de Catalunya y acabé la carrera en Irvine, California,
-       donde pude juntar diariamente mis estudios con mi pasión por el surf. Tras volver a Europa,
+       donde podían convivir diariamente mis estudios con mi pasión por el surf. Tras volver a Europa,
        completé un Máster en Ingeniería en Energías Renovables en Lisboa, donde viví
        un año lleno de emociones. Allí fue donde me uní al grupo de teatro <a href="http://www.lisbonplayers.com.pt/site/">"The Lisbon Players"</a>,
        el grupo de teatro en inglés con más trayectoria de Portugal. Protagonizando dos
        de sus producciones, una obra original "The Stone Flower" y una producción de 
-       "Much Ado About Nothing" de William Shakespeare, pude trabajar con dialectos ingleses
-       y aplicar el conocimiento profesional que tenía hasta entonces del idioma encima de 
+       "Much Ado About Nothing" de William Shakespeare, pude trabajar con diferentes dialectos ingleses
+       y aplicar el conocimiento profesional que tenía del idioma encima de 
        las tablas. Esta experiencia me alentó a perseguir mi sueño, siempre presente, de ser actor.
      </p>
 
      <p>Por ello, volví a Barcelona en 2017, donde cursé la Formación Profesional 
-       del Actor en el Estudi Nancy Tuñón- Jordi Oliver, donde he adquirido nuevos conocimientos
-       tanto sobre mis mecanismos emocionales como actor, como sobre las técnicas
-       necesarias en la interpretación. Mis ganas y voluntad de actuar me han llevado a complementar
+       del Actor en el Estudi Nancy Tuñón-Jordi Oliver, donde he adquirido nuevos conocimientos
+       tanto sobre mis mecanismos emocionales como actor, como sobre técnicas
+       necesarias para la interpretación. Mis ganas y voluntad de actuar me han llevado a complementar
        esta formación con otra que considero más personal y que pasa por consumir teatro, cine,
        leer, escribir, participar en cortometrajes, obras de creación colectiva, cursos
-       complementarios y, en definitiva, preocuparse por crecer como artista día a día.
+       complementarios y, en definitiva, preocuparme por crecer como artista día a día.
        </p>
      
-      <p>El deporte siempre ha estado presente durante mi vida y el surf y el tenis son
+      <p>El deporte siempre ha estado presente durante mi vida, y el surf y el tenis son
         mis dos prácticas favoritas. También escribo poemas, que podéis ver en la sección de
         la web correspondiente, y actualmente llevo dos años trabajando como desarrollador
         web de forma paralela a la formación teatral.</p>
