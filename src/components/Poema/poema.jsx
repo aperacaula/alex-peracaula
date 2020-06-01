6f9cab5c4a71dfbad27122a7c19363af846041cd
Alex <aperacaularuiz@gmail.com>
2020-06-01 20:20:52 +0200

    Poemas done

---
+++ src/components/Poema/poema.jsx
@@ -0,0 +1,20 @@
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import Header from "../Header/header"
+import Footer from "../Footer/footer"
+import styles from "./poema.module.scss"
+
+const Poema = ({ children, title }) => (
+    <div className={styles.poema_container}>
+        <h2>{title}</h2>
+        <div>{children}</div>
+    </div>
+  )
+
+Poema.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Poema
