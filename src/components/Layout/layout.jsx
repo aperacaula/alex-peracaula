f798ac456874b1b21c9381b0b28dae43cddbcd33
Alex <aperacaularuiz@gmail.com>
2020-06-15 23:18:12 +0200

    Fix layout fuckup

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "../Header/header"
 import Footer from "../Footer/footer"
+import styles from "./layout.module.scss"
 
 const Layout = ({ children, page = 'home', slide = false, lan = 'es' }) => {
 
