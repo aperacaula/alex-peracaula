8a9513703308de2c1ca0fcf592c9d47e35c7cf56
Alex <aperacaularuiz@gmail.com>
2020-06-20 21:23:33 +0200

    Typos

--- src/pages/galeria.ca.jsx
+++ src/pages/galeria.ca.jsx
@@ -55,7 +55,7 @@ const Galeria = (props) => {
   return (
     <Layout page={'galeria'} lan='ca'>
       <SEO title="Galeria" lang="ca" description="Galeria de fotografies d'Alex Peracaula"/>
-      <h1>Galeria d'imatges de l'Àlex</h1>
+      <h1>Galeria d'imatges</h1>
       <Masonry
         breakpointCols={breakpointColumnsObj}
         className={styles.my_masonry_grid}
